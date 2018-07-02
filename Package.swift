81f72f934372e54d535177361541d3b12e9af52c
Milan Vit <milan.vit@monstar-lab.com>
2018-07-02 17:29:07 +0900

    Renamed to something more sensible (yet likely temporary)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftLintIntegration",
+    name: "BetterXC",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -16,7 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftLintIntegration",
+            name: "BetterXC",
             dependencies: ["xcproj", "Swiftline", "SwiftShell"]),
     ]
 )
